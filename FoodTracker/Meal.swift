f5c7171efaafadf88a520af391e2c998f1204db6
Yoshua Elmaryono <yoshuaelmaryono@yoshua-e.local>
2018-08-04 16:39:46 +0700

    make meal class conform to nscoding

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     //MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
     var maximumRating = 5
+    //MARK: Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     //MARK: Initializers
     init?(name: String, photo: UIImage?, rating: Int){
@@ -24,5 +31,33 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+        
+        super.init()
+    }
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        guard let rating = aDecoder.decodeObject(forKey: PropertyKey.rating) as? Int else {
+            os_log("Unable to decode the rating for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
